d8bc0db6d0cc5af7eda1ea9bd40deabdb7ba65c6
pupil <pupil@ivkhk.ee>
2020-09-10 10:10:53 +0300

    Работающая программа без рандома

--- src/jptvr19game/App.java
+++ src/jptvr19game/App.java
@@ -5,13 +5,25 @@
  */
 package jptvr19game;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 public class App {
     public void run(){
-        System.out.println("Hello");
+        System.out.println("Привет.");
+        System.out.println("Программа загадала число, угадай:");
+        int myNumber = 2;
+        Scanner scanner = new Scanner(System.in);
+        int gamerNumber = scanner.nextInt();
+        if(myNumber == gamerNumber){
+            System.out.println("Ты выиграл!");
+        }else{
+            System.out.println("Ты проиграл! Было задумано: "
+                + myNumber);
+        }
     }
     
 }
